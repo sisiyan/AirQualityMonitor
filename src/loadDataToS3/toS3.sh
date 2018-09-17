00abd90de1dbdf571d7c8c151927bec78cfc0f0c
Si Yan <syan62@gatech.edu>
2018-09-17 14:36:12 -0400

    new data to s3

--- src/loadDataToS3/toS3.sh
+++ src/loadDataToS3/toS3.sh
@@ -3,20 +3,25 @@
 # Then the script expands all files using bzip2
 # Finally the files are uploaded to S3 to the specific bucket s3://ac-reddit-data/Raw/
 
-mkdir climate_data
-mkdir climate_data_unpack
-cd climate_data
-wget -r -nH -nd -R index.html* https://www.ncei.noaa.gov/data/global-hourly/archive/
-wait
+mdkir /home/ubuntu/data
+mkdir /home/ubuntu/data_unpack
 
-for filename in /home/ubuntu/climate_data/*; do
+wget https://aqs.epa.gov/aqsweb/airdata/download_files.html#Raw/hourly*.zip
+
+# mkdir climate_data
+# mkdir climate_data_unpack
+# cd climate_data
+# wget -r -nH -nd -R index.html* https://www.ncei.noaa.gov/data/global-hourly/archive/
+# wait
+#
+for filename in /home/ubuntu/data/*; do
   tar zxvf "$filename" -C /home/ubuntu/climate_data_unpack
   rm "$filename"
  wait
 done
-
-for filename in /home/ubuntu/climate_data_unpack/*; do
-
-  aws s3 cp "$filename" s3://sy-insight-aq-climate-data/climate_data/
+#
+for filename in /home/ubuntu/data_unpack/*; do
+  aws s3 cp "$filename" s3://sy-insight-aq-climate-data/data/
+  rm "$filename"
   wait
- done
+done
