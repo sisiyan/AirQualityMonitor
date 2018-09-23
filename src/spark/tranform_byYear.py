376e7a1f2f946bae4655a6c0d6133b0b2c76f8ae
Si Yan <syan62@gatech.edu>
2018-09-23 10:31:31 -0400

    combine weather data by year

--- src/spark/tranform_byYear.py
+++ src/spark/tranform_byYear.py
@@ -21,6 +21,13 @@ particulates_codes = ['88101','88502','81102']
 parameter_codes = ['44201', '42401','42101','42602','88101','88502','81102',
     'SPEC','PM10SPEC','WIND','TEMP','PRESS','RH_DP']
 
+#define the dictionary map the file names to schema column names
+schema_dict = {
+    '44201': "ozone", '42401': "SO2",'42101':"CO",'42602':"NO2",
+    '88101': "PM2.5_FRM",'88502':"PM2.5_nonFRM",'81102':"PM10_mass",
+    'SPEC':"PM2.5_speciation",'PM10SPEC': "PM10_speciation",
+    'WIND':"winds",'TEMP':"temperature",'PRESS':"pressure",'RH_DP':"RH_dewpoint"
+}
 
 def convert_to_int(string):
     '''
@@ -97,7 +104,7 @@ for fname in files_year:
     if parameterCode in particulates_codes:
         particulates_files.append(fname)
 
-files_year = ['hourly_TEMP_1999.csv', 'hourly_PRESS_1999.csv']
+weather_files = ['hourly_TEMP_1999.csv', 'hourly_PRESS_1999.csv']
 
 df_join_weather = None
 for fname in weather_files:
@@ -109,11 +116,16 @@ for fname in weather_files:
     if parameterCode == "WIND":
         fdata = fdata.filter(fdata["Parameter Code"] == "61103")
 
-    df = fdata.select('Site Num','Date GMT','Time GMT','Sample Measurement', 'MDL')
+    df = fdata.select('State Name', 'County Name', 'Latitude','Longitude','Date GMT','Time GMT','Sample Measurement','MDL')
+    parameter = schema_dict[parameterCode]
+    df = df.withColumnRenamed("Sample Measurement", parameter).withColumnRenamed("State Name", "state_name").withColumnRenamed("County Name", "county_name").withColumnRenamed("Date GMT", "Date_GMT").withColumnRenamed("Time GMT", "Time_GMT")
+    df = df.withColumn("latitude", df["Latitude"].cast(DoubleType())).withColumn("longitude", df["Longitude"].cast(DoubleType())).withColumn(parameter, df[parameter].cast(DoubleType()))
+
     if df_join_weather == None:
         df_join_weather = df
     else:
-        df_join_weather = df_join_weather.join(df, ["Site Num",'Date GMT','Time GMT'],"outer")
+        df_join_weather = df_join_weather.join(df, ["state_name",'county_name','latitude','longitude','Date_GMT','Time_GMT'],"outer")
+        print df_join_weather.count()
 
-print df_join_weather.count()
+
 print df_join_weather.take(10)
