d584abc5449a963e8dab574ce2ddc56c37e4da44
Si Yan <syan62@gatech.edu>
2018-09-22 21:13:55 -0400

    combine weather data by year

---
+++ src/spark/tranform_byYear.py
@@ -0,0 +1,112 @@
+import sys
+import json
+import os
+
+from pyspark.sql import SparkSession
+import mysql.connector as sqlcon
+from pyspark.sql.types import StructType
+from pyspark.sql.types import StructField
+from pyspark.sql.types import StringType
+from pyspark import SparkConf, SparkContext, SQLContext
+from boto.s3.connection import S3Connection
+from pyspark.sql.types import *
+
+sc = SparkContext()
+sqlContext = SQLContext(sc)
+weather_codes = ['WIND','TEMP','PRESS','RH_DP']
+gases_codes = ['44201', '42401','42101','42602']
+particulates_codes = ['88101','88502','81102']
+
+def convert_to_int(string):
+    '''
+    Returns an integer if it can or returns None otherwise
+    '''
+    try:
+        number = int(string)
+    except ValueError:
+        return None
+    return number
+
+def file_year_paraCode(fname):
+    '''
+    Given the filename XXX_Code_year.extension, return integer year and code
+    '''
+    try:
+        basename = fname.split('.')[0]
+        parameterCode = basename.split('_')[1]
+        if parameterCode == 'RH':
+            year_string = basename.split('_')[3]
+            parameterCode = "RH_DP"
+        else:
+            year_string = basename.split('_')[2]
+    except (ValueError, IndexError):
+        return None, None
+    if parameterCode not in parameter_codes:
+        print parameterCode
+        return None, None
+    year = convert_to_int(year_string)
+    if not year:
+        print parameterCode
+        return None, None
+    return year, parameterCode
+
+
+
+def get_file_list_perYear(bucket_name, target_year):
+    '''
+    Given the S3 bucket, return a list of files in the same year
+    '''
+
+    file_list = []
+
+    conn = S3Connection()
+    bucket = conn.get_bucket(bucket_name)
+
+    for bucket_object in bucket.get_all_keys():
+        fname = bucket_object.key
+
+        if not fname.startswith('hourly') and not fname.startswith('Hourly'):
+            continue
+        year,parameterCode = file_year_paraCode(fname)
+
+        if not year:
+            continue
+        if year == target_year:
+            file_list.append((fname, year))
+
+    return [f[0] for f in file_list]
+
+
+files_year = get_file_list_perYear("sy-insight-epa-data", 1999)
+print files_year
+
+weather_files = []
+gases_files = []
+particulates_files = []
+for fname in files_year:
+    year,parameterCode = file_year_paraCode(fname)
+    if parameterCode in weather_codes:
+        weather_files.append(fname)
+    if parameterCode in gases_codes:
+        gases_files.append(fname)
+    if parameterCode in particulates_codes:
+        particulates_files.append(fname)
+
+
+df_join_weather = None
+for fname in weather_files:
+    year,parameterCode = file_year_paraCode(fname)
+    fdata = sqlContext.read.format('com.databricks.spark.csv').option('header', 'true').load('s3a://sy-insight-epa-data/'+fname)
+
+    if parameterCode == "RH_DP":
+        fdata = fdata.filter(fdata["Parameter Name"] == "Relative Humidity ")
+    if parameterCode == "WIND":
+        fdata = fdata.filter(fdata["Parameter Code"] == "61103")
+
+    df = fdata.select('Site Num','Date GMT','Time GMT','Sample Measurement', 'MDL')
+    if df_join_weather == None:
+        df_join_weather = df
+    else:
+        df_join_weather = df_join_weather.join(df, ["Site Num",'Date GMT','Time GMT'],"outer")
+
+print df_join_weather.count()
