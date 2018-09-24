e6bb0b50efc978424e96a4558839d5c46104a847
Si Yan <syan62@gatech.edu>
2018-09-24 16:15:55 -0400

    join weather and pollutant dataframe, check null values

--- src/spark/tranform_byYear.py
+++ src/spark/tranform_byYear.py
@@ -27,7 +27,7 @@ schema_dict = {
     '44201': "ozone", '42401': "SO2",'42101':"CO",'42602':"NO2",
     '88101': "PM2point5_FRM",'88502':"PM2point5_nonFRM",'81102':"PM10_mass",
     'SPEC':"PM2point5_speciation",'PM10SPEC': "PM10_speciation",
-    'WIND':"winds",'TEMP':"temperature",'PRESS':"pressure",'RH_DP':"Relative_Humidity"
+    'WIND':"winds",'TEMP':"temperature",'PRESS':"pressure",'RH_DP':"relative_humidity"
 }
 
 def convert_to_int(string):
@@ -111,7 +111,8 @@ for fname in files_year:
 df_join_weather = None
 for fname in weather_files:
     year,parameterCode = file_year_paraCode(fname)
-    fdata = sqlContext.read.format('com.databricks.spark.csv').option('header', 'true').load('s3a://sy-insight-epa-data/'+fname)
+    fdata = sqlContext.read.format('com.databricks.spark.csv').option('header', 'true')\
+            .load('s3a://sy-insight-epa-data/'+fname)
 
     if parameterCode == "RH_DP":
         fdata = fdata.filter(fdata["Parameter Name"] == "Relative Humidity ")
@@ -120,30 +121,48 @@ for fname in weather_files:
 
     df = fdata.select('State Name', 'County Name', 'Latitude','Longitude','Date GMT','Time GMT','Sample Measurement')
     parameter = schema_dict[parameterCode]
-    df = df.withColumnRenamed("Sample Measurement", parameter).withColumnRenamed("State Name", "state_name").withColumnRenamed("County Name", "county_name").withColumnRenamed("Date GMT", "Date_GMT").withColumnRenamed("Time GMT", "Time_GMT")
-    df = df.withColumn("latitude", df["Latitude"].cast(DoubleType())).withColumn("longitude", df["Longitude"].cast(DoubleType())).withColumn(parameter, df[parameter].cast(DoubleType()))
+    df = df.withColumnRenamed("Sample Measurement", parameter)\
+        .withColumnRenamed("State Name", "state_name")\
+        .withColumnRenamed("County Name", "county_name")\
+        .withColumnRenamed("Date GMT", "date_GMT")\
+        .withColumnRenamed("Time GMT", "time_GMT")
+    df = df.withColumn("latitude", df["Latitude"].cast(DoubleType()))\
+        .withColumn("longitude", df["Longitude"].cast(DoubleType()))\
+        .withColumn(parameter, df[parameter].cast(DoubleType()))
 
     if df_join_weather == None:
         df_join_weather = df
     else:
-        df_join_weather = df_join_weather.join(df, ["state_name",'county_name','latitude','longitude','Date_GMT','Time_GMT'],"outer")
+        df_join_weather = df_join_weather\
+            .join(df, ["state_name",'county_name','latitude','longitude','date_GMT','time_GMT'],"outer")
 
 
 df_join_gases = None
 for fname in gases_files:
     year,parameterCode = file_year_paraCode(fname)
-    fdata = sqlContext.read.format('com.databricks.spark.csv').option('header', 'true').load('s3a://sy-insight-epa-data/'+fname)
+    fdata = sqlContext.read.format('com.databricks.spark.csv')\
+            .option('header', 'true')\
+            .load('s3a://sy-insight-epa-data/'+fname)
 
     df = fdata.select('State Name', 'County Name', 'Latitude','Longitude','Date GMT','Time GMT','Sample Measurement','MDL')
     parameter = schema_dict[parameterCode]
     parameter_MDL = parameter + "_MDL"
-    df = df.withColumnRenamed("Sample Measurement", parameter).withColumnRenamed("State Name", "state_name").withColumnRenamed("County Name", "county_name").withColumnRenamed("Date GMT", "Date_GMT").withColumnRenamed("Time GMT", "Time_GMT").withColumnRenamed("MDL", parameter_MDL)
-    df = df.withColumn("latitude", df["Latitude"].cast(DoubleType())).withColumn("longitude", df["Longitude"].cast(DoubleType())).withColumn(parameter, df[parameter].cast(DoubleType())).withColumn(parameter_MDL, df[parameter_MDL].cast(DoubleType()))
+    df = df.withColumnRenamed("Sample Measurement", parameter)\
+        .withColumnRenamed("State Name", "state_name")\
+        .withColumnRenamed("County Name", "county_name")\
+        .withColumnRenamed("Date GMT", "date_GMT")\
+        .withColumnRenamed("Time GMT", "time_GMT")\
+        .withColumnRenamed("MDL", parameter_MDL)
+    df = df.withColumn("latitude", df["Latitude"].cast(DoubleType()))\
+        .withColumn("longitude", df["Longitude"].cast(DoubleType()))\
+        .withColumn(parameter, df[parameter].cast(DoubleType()))\
+        .withColumn(parameter_MDL, df[parameter_MDL].cast(DoubleType()))
 
     if df_join_gases == None:
         df_join_gases = df
     else:
-        df_join_gases = df_join_gases.join(df, ["state_name",'county_name','latitude','longitude','Date_GMT','Time_GMT'],"outer")
+        df_join_gases = df_join_gases\
+            .join(df, ["state_name",'county_name','latitude','longitude','date_GMT','time_GMT'],"outer")
 
 """
 df_join_particulates = None
@@ -162,17 +181,18 @@ for fname in particulates_files:
         df_join_particulates = df_join_particulates.join(df, ["state_name",'county_name','latitude','longitude','Date_GMT','Time_GMT'],"outer")
 """
 
-df_join_gases_weather = df_join_weather.join(df_join_gases, ["state_name",'county_name','latitude','longitude','Date_GMT','Time_GMT'], "inner")
-split_date = functions.split(df_join_gases_weather['Date_GMT'], '-')
+df_join_gases_weather = df_join_weather\
+                        .join(df_join_gases, ["state_name",'county_name','latitude','longitude','date_GMT','time_GMT'], "inner")
+split_date = functions.split(df_join_gases_weather['date_GMT'], '-')
 df_join_gases_weather = df_join_gases_weather.withColumn('GMT_year', split_date.getItem(0))
 df_join_gases_weather = df_join_gases_weather.withColumn('GMT_month', split_date.getItem(1))
 df_join_gases_weather = df_join_gases_weather.withColumn('GMT_day', split_date.getItem(2))
 df_join_gases_weather = df_join_gases_weather\
-    .withColumn("Date_GMT", df_join_gases_weather["Date_GMT"].cast(DateType()))\
-    .withColumn("Time_GMT", df_join_gases_weather["Time_GMT"].cast(TimestampType()))\
+    .withColumn("date_GMT", df_join_gases_weather["date_GMT"].cast(DateType()))\
+    .withColumn("time_GMT", df_join_gases_weather["time_GMT"].cast(TimestampType()))\
     .withColumn('GMT_year', df_join_gases_weather['GMT_year'].cast(IntegerType()))\
     .withColumn('GMT_month', df_join_gases_weather['GMT_month'].cast(IntegerType()))\
-    .withColumn('GMT_day', df_join_gases_weather['GMT_day'].cast(IntegerType())))
+    .withColumn('GMT_day', df_join_gases_weather['GMT_day'].cast(IntegerType()))
 
 
 #" And number of null values: " + str(df_join_gases_weather.select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in df_join_gases_weather.columns]).show())
