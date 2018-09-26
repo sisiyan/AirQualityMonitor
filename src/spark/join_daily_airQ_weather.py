6eb3d445f22f9f4ac412682f97ee604349a5fc69
Si Yan <syan62@gatech.edu>
2018-09-26 13:43:29 -0400

    bin data by day

--- src/spark/join_daily_airQ_weather.py
+++ src/spark/join_daily_airQ_weather.py
@@ -206,7 +206,7 @@ def main():
         df_join_gases_weather = df_join_gases_weather\
             .withColumn("date_GMT", df_join_gases_weather["date_GMT"].cast(DateType()))\
             .withColumn('GMT_year', df_join_gases_weather['GMT_year'].cast(IntegerType()))\
-            .withColumn('GMT_month', df_join_gases_weather['GMT_month'].cast(IntegerType()))
+            .withColumn('GMT_month', df_join_gases_weather['GMT_month'].cast(IntegerType()))\
             .dropDuplicates()
 
         # Inner join the weather data and particulate pollutant data
@@ -219,7 +219,7 @@ def main():
         df_join_particulates_weather = df_join_particulates_weather\
             .withColumn("date_GMT", df_join_particulates_weather["date_GMT"].cast(DateType()))\
             .withColumn('GMT_year', df_join_particulates_weather['GMT_year'].cast(IntegerType()))\
-            .withColumn('GMT_month', df_join_particulates_weather['GMT_month'].cast(IntegerType()))
+            .withColumn('GMT_month', df_join_particulates_weather['GMT_month'].cast(IntegerType()))\
             .dropDuplicates()
         print "total number of rows "+ str(df_join_gases_weather.count())
 
