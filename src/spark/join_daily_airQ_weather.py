14ee37bc0e0a07c8b5492b186651091b4cb90a5a
Si Yan <syan62@gatech.edu>
2018-09-26 11:27:01 -0400

    bin data by day

--- src/spark/join_daily_airQ_weather.py
+++ src/spark/join_daily_airQ_weather.py
@@ -114,13 +114,26 @@ def average_over_day(fname):
                     .agg({'Sample Measurement': 'mean'})
     return df_dailyBin
 
+def rename_cols(df, parameter_avg):
+    df = df.withColumnRenamed("avg(Sample Measurement)", parameter_avg)\
+        .withColumnRenamed("State Name", "state_name")\
+        .withColumnRenamed("County Name", "county_name")\
+        .withColumnRenamed("Date GMT", "date_GMT")
+
+    df = df.withColumn("latitude", df["Latitude"].cast(DoubleType()))\
+        .withColumn("longitude", df["Longitude"].cast(DoubleType()))\
+        .withColumn(parameter_avg, df[parameter_avg].cast(DoubleType()))
+
+    return df
+
+
 def main():
     files_per_year = get_file_list_perYear("sy-insight-epa-data", 1980)
     # for fname in files_per_year
-    df_dailyBin = average_over_day("hourly_42101_1980.csv")
-    print df_dailyBin.filter((df_dailyBin["Latitude"] == 33.520661) & (df_dailyBin["Longitude"] == -86.801934) & (df_dailyBin["Date GMT"] == "1980-01-01")).show()
+    # df_dailyBin = average_over_day("hourly_42101_1980.csv")
+    # print df_dailyBin.filter((df_dailyBin["Latitude"] == 33.520661) & (df_dailyBin["Longitude"] == -86.801934) & (df_dailyBin["Date GMT"] == "1980-01-01")).show()
 
-    """
+
     weather_files, gases_files, particulates_files = classify_files(files_per_year)
 
     # Outer Join weather data
@@ -134,22 +147,72 @@ def main():
         if parameterCode == "WIND":
             fdata = fdata.filter(fdata["Parameter Code"] == "61103")
 
-        parameter = schema_dict[parameterCode] + "_avg"
-        df = df.withColumnRenamed("avg(Sample Measurement)", parameter)\
-            .withColumnRenamed("State Name", "state_name")\
-            .withColumnRenamed("County Name", "county_name")\
-            .withColumnRenamed("Date GMT", "date_GMT")\
-            .withColumnRenamed("Time GMT", "time_GMT")
-        df = df.withColumn("latitude", df["Latitude"].cast(DoubleType()))\
-            .withColumn("longitude", df["Longitude"].cast(DoubleType()))\
-            .withColumn(parameter, df[parameter].cast(DoubleType()))
+        parameter_avg = schema_dict[parameterCode] + "_avg"
+        df = rename_cols(df, parameter_avg)
 
         if df_join_weather == None:
             df_join_weather = df
         else:
             df_join_weather = df_join_weather\
-                .join(df, ["state_name",'county_name','latitude','longitude','date_GMT','time_GMT'],"outer")
-        """
+                .join(df, ["state_name",'county_name','latitude','longitude','date_GMT'],"outer")
+
+    # Outer join all gasese pollutant data
+    df_join_gases = None
+    for fname in gases_files:
+        year,parameterCode = file_year_paraCode(fname)
+        df = average_over_day(fname)
+        parameter_avg = schema_dict[parameterCode] + "_avg"
+        df = rename_cols(df, parameter_avg)
+
+        if df_join_gases == None:
+            df_join_gases = df
+        else:
+            df_join_gases = df_join_gases\
+                .join(df, ["state_name",'county_name','latitude','longitude','date_GMT'],"outer")
+
+    # Outer join all particulate pollutant data
+    df_join_particulates = None
+    for fname in particulates_files:
+        year,parameterCode = file_year_paraCode(fname)
+
+        if parameterCode == 'SPEC' or parameterCode == 'PM10SPEC':
+            continue
+
+        df = average_over_day(fname)
+        parameter_avg = schema_dict[parameterCode] + "_avg"
+        df = rename_cols(df, parameter_avg)
+
+        if df_join_particulates == None:
+            df_join_particulates = df
+        else:
+            df_join_particulates = df_join_particulates.join(df, ["state_name",'county_name','latitude','longitude','date_GMT'],"outer")
+
+    # Inner join the weather data and gas pollutant data
+    df_join_gases_weather = df_join_weather\
+                            .join(df_join_gases, ["state_name",'county_name','latitude','longitude','date_GMT'], "inner")
+    split_date = functions.split(df_join_gases_weather['date_GMT'], '-')
+    df_join_gases_weather = df_join_gases_weather.withColumn('GMT_year', split_date.getItem(0))
+    df_join_gases_weather = df_join_gases_weather.withColumn('GMT_month', split_date.getItem(1))
+
+    df_join_gases_weather = df_join_gases_weather\
+        .withColumn("date_GMT", df_join_gases_weather["date_GMT"].cast(DateType()))\
+        .withColumn('GMT_year', df_join_gases_weather['GMT_year'].cast(IntegerType()))\
+        .withColumn('GMT_month', df_join_gases_weather['GMT_month'].cast(IntegerType()))
+
+    # Inner join the weather data and particulate pollutant data
+    df_join_particulates_weather = df_join_weather\
+                            .join(df_join_particulates, ["state_name",'county_name','latitude','longitude','date_GMT'], "inner")
+    split_date = functions.split(df_join_particulates_weather['date_GMT'], '-')
+    df_join_particulates_weather = df_join_particulates_weather.withColumn('GMT_year', split_date.getItem(0))
+    df_join_particulates_weather = df_join_particulates_weather.withColumn('GMT_month', split_date.getItem(1))
+
+    df_join_particulates_weather = df_join_particulates_weather\
+        .withColumn("date_GMT", df_join_particulates_weather["date_GMT"].cast(DateType()))\
+        .withColumn('GMT_year', df_join_particulates_weather['GMT_year'].cast(IntegerType()))\
+        .withColumn('GMT_month', df_join_particulates_weather['GMT_month'].cast(IntegerType()))
+
+    print "total number of rows "+ str(df_join_gases_weather.count())
+    print df_join_gases_weather.show()
 
 if __name__ == '__main__':
     main()
