e27006cad5e9cfaed6a5e25369340890d999b2a9
Si Yan <syan62@gatech.edu>
2018-10-01 11:29:10 -0400

    test airflow

--- src/airflow/control.py
+++ src/airflow/control.py
@@ -24,30 +24,31 @@ task_year = 1980
 now = datetime.now()
 current_year = now.year
 
-# while task_year <= current_year:
-#     '''
-#     Run the batch processes year by year
-#     '''
+while task_year <= current_year:
+    '''
+    Run the batch processes year by year
+    '''
 
-t1 = BashOperator(
-    task_id='download_{}'.format(task_year),
-    bash_command='python /home/ubuntu/insightProject/src/loadDataToS3/download_toLocal.py {{params.task_year}}',
-    params={'task_year': str(task_year)},
-    dag=dag)
+    t1 = BashOperator(
+        task_id='download_{}'.format(task_year),
+        bash_command='python /home/ubuntu/insightProject/src/loadDataToS3/download_toLocal.py {{params.task_year}}',
+        params={'task_year': str(task_year)},
+        dag=dag)
 
-t2 = BashOperator(
-    task_id='upload_{}_csv_toS3'.format(task_year),
-    bash_command='/home/ubuntu/insightProject/src/loadDataToS3/uploadToS3.sh',
-    dag=dag)
+    t2 = BashOperator(
+        task_id='upload_{}_csv_toS3'.format(task_year),
+        bash_command='/home/ubuntu/insightProject/src/loadDataToS3/uploadToS3.sh',
+        dag=dag)
 
-t3 = BashOperator(
-    task_id='process_{}'.format(task_year),
-    bash_command='/home/ubuntu/insightProject/src/spark/run_join_airQ_weather.sh {{params.task_year}}',
-    params={'task_year': str(task_year)},
-    dag=dag)
-#
-t2.set_upstream(t1)
-t3.set_upstream(t2)
+    t3 = BashOperator(
+        task_id='process_{}'.format(task_year),
+        bash_command='/home/ubuntu/insightProject/src/spark/run_join_airQ_weather.sh {{params.task_year}}',
+        params={'task_year': str(task_year)},
+        dag=dag)
+    #
+    t2.set_upstream(t1)
+    t3.set_upstream(t2)
+    task_year = task_year + 1
 
 # t4 = BashOperator(
 #     task_id='update_db',
