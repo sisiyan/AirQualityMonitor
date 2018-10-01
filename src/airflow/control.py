bba0bafaf3fa78ac29832d30042ccafa8d1d04ce
Si Yan <syan62@gatech.edu>
2018-10-01 14:04:18 -0400

    test airflow

--- src/airflow/control.py
+++ src/airflow/control.py
@@ -37,7 +37,7 @@ while task_year <= 1980:
 
     t2 = BashOperator(
         task_id='upload_{}_csv_toS3'.format(task_year),
-        bash_command='cd /home/ubuntu/insightProject/src/loadDataToS3/; ./uploadToS3.sh',
+        bash_command='cd /home/ubuntu/insightProject/src/loadDataToS3/; ./uploadToS3.sh ',
         dag=dag)
 
     t3 = BashOperator(
